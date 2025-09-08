81e32f1da4b65cc90ec5dedc831c77be2d0a5459
Anurag Pandey <anurag077269@gmail.com>
2025-09-08 20:18:33 +0530

    Working on API Server

--- api/main.py
+++ api/main.py
@@ -1,1 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from io import BytesIO
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "Hello, i am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    data
+
+@app.post("/predict")
+async def predict (
+    file: UploadFile = File(...)
+):
+    bytes = read_file_as_image(await file.read())
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
